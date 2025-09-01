28053b20149cf4661b2c70dd0a745ae3ed785c58
SienaL2023 <xiaosiena@gmail.com>
2025-08-31 17:59:16 -0700

    simplified debts

--- src/App.java
+++ src/App.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+import java.util.PriorityQueue;
 
 public class App {
     public static void main(String[] args) throws Exception {
@@ -13,8 +13,8 @@ public class App {
         // each person owe to each and every person
         
         List<String> friends = Arrays.asList("Alice", "Bob", "Carol", "Dave", "Eve");
-        String[] payers = {"Alice", "Bob", "Bob", "Carol", "Alice"};
-        double[] amounts = {150.0, 200.0, 180.0, 220.0, 250.0};
+        String[] payers = {"Eve", "Bob", "Alice", "Carol", "Dave"};
+        double[] amounts = {180.0, 250.0, 180.0, 200.0, 250.0};
 
         // everyone spends equal amounts
         // day 1
@@ -31,7 +31,7 @@ public class App {
         // dave -70     (-40)
         // eve -70      (-40)
 
-        Map<String, Double> balances = new HashMap<>();
+        HashMap<String, Double> balances = new HashMap<>();
 
         for(String friend: friends){
             balances.put(friend, 0.0);
@@ -58,6 +58,80 @@ public class App {
         System.out.println("Net balances: ");
         for(String friend:friends){
             System.out.println(friend + " " + balances.get(friend));
+        }
+
+        System.out.println("Simplified debts");
+        simplifyDebts(balances);
+
+        // simplify the expenses
+        // end goal: the payers get their money back
+        // creditors: 
+        // debtors:
+        // find the largest creditor and the smallest debtor and pay out
+        // alice - dave --> alice: 0, dave: 0
+        // Alice 0.0
+        // Bob 180.0
+        // Carol 20.0
+        // Dave 0.0
+        // Eve -200.0
+
+        // bob - eve --> bob: 0, eve: -20
+
+        // Alice 0.0
+        // Bob 0.0
+        // Carol 20.0
+        // Dave 0.0
+        // Eve -20.0
+
+    }
+
+    static void simplifyDebts(HashMap <String, Double> balances){
+        // finds largest creditor and smallest debtor
+        // when  b.amount is first in (a,b) u r looking for the larger amount
+        PriorityQueue <Person> creditors = new PriorityQueue<>((a,b) -> Double.compare(b.amount, a.amount));
+        PriorityQueue<Person> debtors = new PriorityQueue<>((a,b) -> Double.compare(a.amount, b.amount));
+
+        // entry -> each pair (key/value pair)
+        // offer -> adds into priority queue
+
+        // same thing as for(String name: names)
+        for(HashMap.Entry<String, Double> entry: balances.entrySet()){
+            String name = entry.getKey();
+            double amount = entry.getValue();
+            if(amount > 0){
+                creditors.offer(new Person(name, amount));
+            }
+
+            else if(amount < 0){
+                debtors.offer(new Person(name, -amount));
+            }
+        }
+        while(!creditors.isEmpty() && !debtors.isEmpty()){
+            Person creditor = creditors.poll();
+            Person debtor = debtors.poll();
+
+            double min = Math.min(creditor.amount, debtor.amount);
+            System.out.printf("%s owes %s: $%.2f\n", debtor.name, creditor.name, min);
+
+            // so if zero then u won't be readded into the hashmap
+            if(creditor.amount > min){
+                creditors.offer(new Person(creditor.name, creditor.amount - min));
+            }
+            if(debtor.amount > min){
+                debtors.offer(new Person(debtor.name, debtor.amount - min));
+            }
+        }
+            
+    }
+
+    // need new type bc it has to hold both string and double
+    static class Person{
+        String name;
+        double amount;
+
+        Person(String name, double amount){
+            this.name = name;
+            this.amount = amount;
         }
     }
 }
